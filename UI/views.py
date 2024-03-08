7e9f2588cd2496cc25f3095cff36ec79348f08c4
Akash Santhosh <akashsanthosh0010@gmail.com>
2024-03-08 20:15:36 +0530

    design added

--- UI/views.py
+++ UI/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-def checker(request):
+def register(request):
     return render(request,'register.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def home(request):
+    return render(request, 'home.html')
